5e899d696edeea81d1dfcb857643cb3e2af375b8
willstenzel <stenzel.will@gmail.com>
2021-05-19 15:51:50 -0600

    Mockup code for indivdual singup

--- src/functions/individual_signup/individual_signup.ts
+++ src/functions/individual_signup/individual_signup.ts
@@ -8,22 +8,62 @@ type EventType = {
     body: FormResponseType;
 }
 
+const PEOPLE_DB_ID = 'a58fd4c21e2b49a1958e4cdc003fdea8';
+
 export default async (event: EventType): Promise<any> => {
-    const response = JSON.stringify(event, null, 2);
-    console.log(event);
+    const { Client } = require('@notionhq/client');
+    const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
     const { name, email, heardAbout } = event.body;
 
-    // Check that the user does not already exist, if they already exist, do nothing
+    // Check if the user is already in the database
+    const peopleQuery = await notion.databases.query({
+        database_id: PEOPLE_DB_ID,
+        filter: {
+            property: 'Email',
+            text: {
+                equals: email,
+            },
+        }
+    });
+
+    // If the person already exists in the db, do nothing
+    if (peopleQuery.length > 0) {
+        return;
+    }
 
     // Create a new person page with given name and email
+    const newPerson = await notion.pages.create({
+        parent: {
+          database_id: '',
+        },
+        properties: {
+          Name: {
+            title: [
+              {
+                text: {
+                  content: name,
+                },
+              },
+            ],
+          },
+          Email: {
+            text: [
+              {
+                text: {
+                  content: email,
+                },
+              },
+            ],
+          }
+        }
+    });
 
+    
     // Get the person notion page url
-
-    // Share the notion page with the given person
+    const newPersonPageID = newPerson.id;
 
     // Send them mail chimp email with given params
     // sendIndividualConfirmation(name, email, notionPersonalPageURL)
-    
-    return response;
+
 }
